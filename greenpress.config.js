3b16a08567f4b00ef85ba749a67894ab4a0793f0
David Meir-Levy <davidmeirlevy@gmail.com>
2021-01-25 18:03:12 +0200

    upgrading blog-front to check be issues

--- greenpress.config.js
+++ greenpress.config.js
@@ -1,9 +1,11 @@
 const path = require('path');
+const IS_PROD = process.env.NODE_ENV !== 'development'
 
 module.exports = {
 	services: {
 		front: {
-			theme: 'global:' + path.join(process.cwd(), 'themes', 'damal2')
+			theme: 'global:' + path.join(process.cwd(), 'themes', 'damal2'),
+			script: process.env.FRONT_SERVICE_SCRIPT || (IS_PROD ? 'npm start' : 'npm run build && npm run dev')
 		}
 	}
 }
