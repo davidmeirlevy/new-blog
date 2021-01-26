015bedca8e61a1bc793eaaceb959b94ac3829e10
David Meir-Levy <davidmeirlevy@gmail.com>
2021-01-26 12:22:47 +0200

    wip

--- greenpress.config.js
+++ greenpress.config.js
@@ -3,7 +3,7 @@ const path = require('path');
 module.exports = {
 	services: {
 		front: {
-			theme: 'global:' + path.join(process.cwd(), 'themes', 'damal2'),
+			theme: 'global:' + path.join(__dirname, 'themes', 'damal2'),
 		}
 	}
 }
