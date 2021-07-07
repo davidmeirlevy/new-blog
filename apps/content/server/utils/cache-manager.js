25a677e6b2fd32d80ae421537a2268d86ca162d8
David Meir-Levy <davidmeirlevy@gmail.com>
2021-07-07 15:02:13 +0300

    test heroku

--- apps/content/server/utils/cache-manager.js
+++ apps/content/server/utils/cache-manager.js
@@ -1,5 +1,7 @@
 const { redisUrl } = require('../../config')
 
+console.log('use cache from redis: ', !!redisUrl)
+
 const cacheManager = require('cache-manager').caching({
   store: redisUrl ? require('cache-manager-redis') : 'memory',
   url: redisUrl,
